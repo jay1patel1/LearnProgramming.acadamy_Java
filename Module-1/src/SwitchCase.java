58db7bcb4095ce04d7a0fd2ed6cfbbcb7b4c25fd
jay1patel1 <140941387+jay1patel1@users.noreply.github.com>
2024-02-27 23:52:47 +0530

    updated

--- Module-1/src/SwitchCase.java
+++ Module-1/src/SwitchCase.java
@@ -6,54 +6,54 @@ public class SwitchCase {
         int month = sc.nextInt();
 
         switch (month) {
-            case 1:
+            case 1 -> {
                 System.out.println("January");
                 System.out.println("31 Days");
-                break;
-            case 2:
+            }
+            case 2 -> {
                 System.out.println("February");
                 System.out.println("28 Days");
-                break;
-            case 3:
+            }
+            case 3 -> {
                 System.out.println("March");
                 System.out.println("31 Days");
-                break;
-            case 4:
+            }
+            case 4 -> {
                 System.out.println("April");
                 System.out.println("30 Days");
-                break;
-            case 5:
+            }
+            case 5 -> {
                 System.out.println("May");
                 System.out.println("31 Days");
-                break;
-            case 6:
+            }
+            case 6 -> {
                 System.out.println("Jun");
                 System.out.println("30 Days");
-                break;
-            case 7:
+            }
+            case 7 -> {
                 System.out.println("July");
                 System.out.println("31 Days");
-                break;
-            case 8:
+            }
+            case 8 -> {
                 System.out.println("August");
                 System.out.println("31 Days");
-                break;
-            case 9:
+            }
+            case 9 -> {
                 System.out.println("September");
                 System.out.println("30Days");
-                break;
-            case 10:
+            }
+            case 10 -> {
                 System.out.println("October");
                 System.out.println("31 Days");
-                break;
-            case 11:
+            }
+            case 11 -> {
                 System.out.println("November");
                 System.out.println("30 Days");
-                break;
-            case 12:
+            }
+            case 12 -> {
                 System.out.println("December");
                 System.out.println("31 Days");
-                break;
+            }
         }
     }
 }
