8508bcd863f13097f06e56687a882455cfec683a
jay1patel1 <140941387+jay1patel1@users.noreply.github.com>
2024-02-28 21:44:17 +0530

    update

--- Module-2/src/randomGame.java
+++ Module-2/src/randomGame.java
@@ -1,5 +1,16 @@
 public class randomGame {
     public static void main(String[] args) {
 
+        int randomValue = 0;
+
+        while (randomValue != 5) {
+            randomValue = (int) (Math.random() * 10) + 1;
+
+            if ((randomValue%2) == 0) {
+                continue;
+            }
+            System.out.println(randomValue);
+        }
+
     }
 }
