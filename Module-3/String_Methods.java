7118394c95d6cbe0ec9025dba1e02b86655e2f0e
jay1patel1 <140941387+jay1patel1@users.noreply.github.com>
2024-03-02 21:41:53 +0530

    updated

--- Module-3/String_Methods.java
+++ Module-3/String_Methods.java
@@ -11,5 +11,20 @@ public class String_Methods {
         System.out.println(str.length());
 
         System.out.println(str.indexOf("lo W"));
+
+        System.out.println(str.contains("llo"));
+
+        System.out.println(str.toLowerCase());
+
+        System.out.println(str.toUpperCase());
+
+        // System.out.println(str.isEmpty(true));
+
+        String abc = "I love programming.";
+        String cba = abc.replace("programming", "java");
+        System.out.println(abc + cba);
+
+        String abc1 = abc.substring(3,7);
+        System.out.println(abc1);
     }
 }
