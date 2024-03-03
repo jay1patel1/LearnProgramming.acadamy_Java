79ea607abd4b9f4c5f05886e8b988d99506c74db
jay1patel1 <140941387+jay1patel1@users.noreply.github.com>
2024-03-03 18:39:07 +0530

    updated

---
+++ Module-4/methods.java
@@ -0,0 +1,11 @@
+/**
+ * methods
+ */
+public class methods {
+    public static void main(String[] args) {
+        System.out.println("Boil Water with tea leaves and sugar");
+        System.out.println("Make tea");
+
+        System.out.println("");
+    }
+}
